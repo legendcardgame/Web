a12c1fd8606b71b2cb378006d82563ddaf60cc86
legendcardgame <legendarycardgame9@gmail.com>
2025-08-14 05:27:54 -0600

    Update config.js

--- preregistro/js/config.js
+++ preregistro/js/config.js
@@ -1,7 +1,8 @@
 // /preregistro/js/config.js
 export const API_BASE =
 
-  "https://script.google.com/macros/s/AKfycbww6hUZ3P0p1QKzrFAjitYv6RSi_z5tbB_At-cS3-wShPWTbSahUe5FSKsXyCiS5jW5AA/exec";
+  "https://script.google.com/macros/s/AKfycbwy6I3fXa0VZaX2AS-e1Jmd8BaB6I9whZjXUec8kUUt4Y0BXjYypZoHw8tYIDZgzJm6ww/exec";
+
 
 
 
