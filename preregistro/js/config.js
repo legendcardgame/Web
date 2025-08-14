2955302b4d06f3bbf36ee6b078651cd4e2743f4f
legendcardgame <legendarycardgame9@gmail.com>
2025-08-14 09:13:53 -0600

    Update config.js

--- preregistro/js/config.js
+++ preregistro/js/config.js
@@ -3,10 +3,4 @@ export const API_BASE =
 
   "https://script.google.com/macros/s/AKfycbw3ATqqHV95BymSvzO7zrT8n_e_clKeGzukPaxF9BWGsSPOl6Lr2QarArM3Y4FVSKn0mQ/exec";
 
-
-
-
-
-
-
-
+export const ADMIN_KEY = 'FFG-201';
