ff95f657fb2ecd7b1964818e8542560c58188d13
legendcardgame <legendarycardgame9@gmail.com>
2025-08-18 20:05:37 -0600

    Update config.js

--- preregistro/utilities/js/config.js
+++ preregistro/utilities/js/config.js
@@ -2,3 +2,5 @@ window.LCG = window.LCG || {};
 window.LCG.API_BASE  = 'https://script.google.com/macros/s/AKfycbxqy6dO1RCRW_oj3O4SFXvU-Y-B5a8nxmK9Zo_lnKOadZJY5vKGF38IRCcj80pqtPA/exec';
 window.LCG.ADMIN_KEY = 'FFG-201';
 
+// 👇 Nuevo: URL de la herramienta de pareos
+window.LCG.PAREOS_URL = 'https://legendcardgame.github.io/Web/OTS/index.html';
